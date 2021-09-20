114eab8e889aa6b97e4b72b16c60995058caed47
stinekamau <60697895+stinekamau@users.noreply.github.com>
2021-09-20 21:16:46 +0300

    conform to the cors policy (send only simple request)

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app=FastAPI()
 
+origins=['http://localhost','http://localhost:8080','http://localhost:*','http.localhost:2715']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex='http://.*'
+
+)
+
 @app.get('/')
 def home():
-    print('Hello')
-    return 'identification'
+    return json.dumps({'type':'identification'})
 
 @app.get('/home')
 def some():
-    return {'detail':'Niice'}
+    return json.dumps({'type':'passport'})
+
 
 @app.get('/images/something')
 def process(image):
--- frontend/launcher.js
+++ frontend/launcher.js
@@ -15,21 +15,43 @@ image_input.addEventListener("change", function() {
    reader.readAsDataURL(this.files[0]);
 });
 
-const http = new XMLHttpRequest()
+   const http = new XMLHttpRequest()
+   http.setRequestHeader('Accept','x-www-form-urlencoded');
+   http.setRequestHeader('Content-Language','en-US');
+   http.setRequestHeader('Accept-Language','en');
+   http.setRequestHeader('Content-Yype','application/x-www-form-urlencoded')
 
-   http.open("GET", "http://127.0.0.1:8000/home")
-   http.send()
+   http.open("GET", "http://127.0.0.1:8000/home");
+   http.send();
+
+   http.onreadystatechange = function() {
+            
+      if(http.readyState == 4) {
+         console.log("Value of response is hehe  : "+http.response)
+         document.getElementById('ans').value = http.status;
+
+         console.log("Something Happened");
+      }
+   }
    
-   http.onload = ()=>{
-      console.log("Just inside the onLoad");
-      // console.log(xhttp.responseText)
-      // console.log("something  on the onload")
-      // document.getElementById("ans").ans = xhttp.responseText;
-      if (http.status != 200) { // analyze HTTP status of the response
+   http.onload = function() {
+      let responseObj = http.response;
+      alert(responseObj.message); 
+      console.log("Value of responseObj "+responseObj)
+      // Hello, world!
+    };
+    console.log("End of the function");
+   
+   // http.onload = ()=>{
+   //    console.log("Just inside the onLoad");
+   //    // console.log(xhttp.responseText)
+   //    // console.log("something  on the onload")
+   //    // document.getElementById("ans").ans = xhttp.responseText;
+   //    if (http.status != 200) { // analyze HTTP status of the response
       
-         alert(`Error ${http.status}: ${http.statusText}`); // e.g. 404: Not Found
-       } else { // show the result
-         alert(`Done, got ${http.response.length} bytes`); // response is the server response
-       }
-   };
+   //       alert(`Error ${http.status}: ${http.statusText}`); // e.g. 404: Not Found
+   //     } else { // show the result
+   //       alert(`Done, got ${http.response.length} bytes`); // response is the server response
+   //     }
+   // };
    
