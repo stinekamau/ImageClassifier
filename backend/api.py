50930c77382c3b68893e8ab1ac63a224c5a8c3e1
stinekamau <60697895+stinekamau@users.noreply.github.com>
2021-10-10 13:21:03 +0300

    cleared unused code

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json
-from pydantic import BaseModel
-from typing import Optional
 from fastapi import Form, Depends
 from checkImage import CascadeClassifier
 from fastapi import UploadFile,File
@@ -23,11 +21,6 @@ app.add_middleware(
 def home():
     return json.dumps({"type":"identification"})
 
-@app.get('/home')
-def some():
-
-    return "Type is passport"
-
 @app.post('/home/process')
 async def image_type(file:UploadFile=File(...)):
     
@@ -35,25 +28,12 @@ async def image_type(file:UploadFile=File(...)):
     with open(pt_name,'wb+') as f:
         f.write(file.file.read())
         f.close()
-
-
-    print(f'Value of item is {file.file}')
     c=CascadeClassifier(str(pt_name))
     output=c.process()
     print(f'Output is {output} ')
     return output['type_']
    
 
-@app.post('/test')
-async def rad():
-    return {'success':'Over'}
-
-@app.post('/images/something')
-def process(image):
-    c=CascadeClassifier(image)
-    output=c.process()
-    print(f'Value of output is {output}')
-    return output 
 run(app)
 
 
